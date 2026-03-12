71fc5a2803950f51d79701f75f63f58221f4b71f
sungyun98 <62874305+sungyun98@users.noreply.github.com>
2026-03-12 08:45:10 +0000

    Deploying to gh-pages from @ sungyun98/sungyun98.github.io@c6d50b2ccb1ec90b7448c6f392c0f2bd9c872539 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "To be updated soon...",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
